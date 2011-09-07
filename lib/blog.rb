a0862b273d31d078d0e754d217b045e918f8c065
charlton objero <charlton@charlton-VirtualBox.(none)>
2011-09-07 18:15:50 -0400

    Running version

--- lib/blog.rb
+++ lib/blog.rb
@@ -3,10 +3,10 @@
 #
 class Blog
 	def initialize new_url
-		@new_url = url
+		@url = new_url
 	end
 	def latest_title
-		new_html = Net::HTTP.get(URI.parse(@new_url))
+		new_html = Net::HTTP.get(URI.parse(@url))
 		doc = Hpricot(new_html)		
              (doc/"/html/body/div[2]/div/div[2]/div/div/h3").inner_html	
 	end

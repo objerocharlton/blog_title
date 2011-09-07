1ba3b856b0a485a77a3d4354adaa5ebd14d10a51
charlton objero <charlton@charlton-VirtualBox.(none)>
2011-09-07 16:56:58 -0400

    Assignment 09/07/2011

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,3 +1,14 @@
 #
 # Create your "Blog" class in this file.
 #
+class Blog
+	def initialize new_url = nil
+		@new_url = new_url
+	end
+	def latest_title
+		new_html = Net::HTTP.get(URI.parse(@new_url))
+		doc = Hpricot(new_html)		
+             (doc/"/html/body/div[2]/div/div[2]/div/div/h3").inner_html	
+	end
+end
+
